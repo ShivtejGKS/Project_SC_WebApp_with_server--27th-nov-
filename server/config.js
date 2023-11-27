24bf7e00a213ed293d210bb08e408bf82c79dd63
ShivtejGKS <shivtej.gks@gmail.com>
2023-11-27 16:34:10 +0530

    first commit

---
+++ server/config.js
@@ -0,0 +1,8 @@
+// server/config.js
+module.exports = {
+    host: 'localhost',
+    port: 3306,  // Change this if your MySQL server is running on a different port
+    user: 'root',
+    password: 'Shiva.123',
+    database: 'project_management'
+};
